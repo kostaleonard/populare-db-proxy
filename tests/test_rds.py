2339d9776c8867e04898b10311909c66f723bff3
Leo Kosta <kostaleonard@gmail.com>
2022-06-17 12:11:48 -0400

    Fixed linting
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

--- tests/test_rds.py
+++ tests/test_rds.py
@@ -3,6 +3,7 @@
 
 def test_rds_creation(mocked_rds: None) -> None:
     """Tests that the mocked RDS was created successfully."""
+    # pylint: disable=unused-argument
     # The mocked RDS instance is created when this test is run.
     # If it was created successfully, there will be no errors in test setup.
     assert True
