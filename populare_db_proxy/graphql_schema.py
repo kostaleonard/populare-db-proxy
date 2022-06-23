006ccc5ce9f4953fadf653192d70d3d4601d2845
Leo Kosta <kostaleonard@gmail.com>
2022-06-23 07:26:51 -0400

    Added basic graphql schema
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

--- populare_db_proxy/graphql_schema.py
+++ populare_db_proxy/graphql_schema.py
@@ -3,3 +3,42 @@
 See the guide here for more information:
 https://docs.graphene-python.org/projects/sqlalchemy/en/latest/tutorial/
 """
+
+from __future__ import annotations
+from datetime import datetime
+from graphene import ObjectType, String, Int, DateTime, Schema
+
+
+class Query(ObjectType):
+    """Represents available GraphQL queries."""
+
+    read_posts = String(
+        limit=Int(required=False),
+        before=DateTime(required=False)
+    )
+
+    def resolve_read_posts(
+            self,
+            info,
+            limit: int | None,
+            before: datetime | None
+    ) -> str:
+        """Returns the response to a read_posts query.
+
+        :param info: The GraphQL context.
+        :param limit: The maximum number of posts to return from the database.
+        :param before: If supplied, return posts created earlier than this
+            date; if None, return the most recent posts (`before` is set to
+            datetime.now()).
+        """
+        # TODO fix docstring
+        # TODO why is the first arg in the guide not self--should this be static?
+        # TODO what are the types of the root and info args?
+
+
+def get_schema() -> Schema:
+    """Returns the GraphQL schema for the proxy.
+
+    :return: The GraphQL schema for the proxy.
+    """
+    return Schema(query=Query)
