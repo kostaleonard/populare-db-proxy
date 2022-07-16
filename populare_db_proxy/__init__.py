9c013780cdc2d592df016d986fcf60cc592afb35
Leo Kosta <kostaleonard@gmail.com>
2022-07-16 09:13:24 -0400

    Closes #27
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

--- populare_db_proxy/__init__.py
+++ populare_db_proxy/__init__.py
@@ -1,4 +1,4 @@
 """Contains source modules."""
 
 
-__version__ = "0.0.7"
+__version__ = "0.0.8"
--- populare_db_proxy/proxy.py
+++ populare_db_proxy/proxy.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_graphql import GraphQLView
 from populare_db_proxy.graphql_schema import get_schema
 from populare_db_proxy.app_data import app
+from populare_db_proxy.db_ops import init_db_schema
 
 
 @app.route("/health")
@@ -20,6 +21,7 @@ def create_app() -> Flask:
 
     :return: The Flask app.
     """
+    init_db_schema()
     app.add_url_rule("/graphql", view_func=GraphQLView.as_view(
         "graphql",
         schema=get_schema(),
